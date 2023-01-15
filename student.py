1d7d2ad915baabfc5e521806fbab7873d246543a
Supriyo-Mandal <89782123+Supriyo-Mandal@users.noreply.github.com>
2023-01-15 10:23:54 +0530

    Update student.py

--- student.py
+++ student.py
@@ -6,8 +6,40 @@ from PIL import Image, ImageTk
 class Student:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1920x1080+0+0")
+        self.root.geometry("1530x790+0+0")
         self.root.title("STUDENT MANAGEMENT SYSTEM")
+
+        # 1st
+        img = Image.open("./college_images/7th.jpg")
+        img = img.resize((540, 160), Image.ANTIALIAS)
+        self.photoimg = ImageTk.PhotoImage(img)
+
+        self.btn_1 = Button(self.root, image=self.photoimg, cursor="hand2")
+        self.btn_1.place(x=0, y=0, width=540, height=160)
+
+        # 2nd
+        img_2 = Image.open("./college_images/6th.jpg")
+        img_2 = img_2.resize((540, 160), Image.ANTIALIAS)
+        self.photoimg_2 = ImageTk.PhotoImage(img_2)
+
+        self.btn_2 = Button(self.root, image=self.photoimg_2, cursor="hand2")
+        self.btn_2.place(x=540, y=0, width=540, height=160)
+
+        # 3rd
+        img_3 = Image.open("./college_images/5th.jpg")
+        img_3 = img_3.resize((540, 160), Image.ANTIALIAS)
+        self.photoimg_3 = ImageTk.PhotoImage(img_3)
+
+        self.btn_3 = Button(self.root, image=self.photoimg_3, cursor="hand2")
+        self.btn_3.place(x=1000, y=0, width=540, height=160)
+
+        # bg image
+        img_4 = Image.open("./college_images/university.jpg")
+        img_4 = img_4.resize((1530, 710), Image.ANTIALIAS)
+        self.photoimg_4 = ImageTk.PhotoImage(img_4)
+
+        bg_label = Label(self.root, image=self.photoimg_4, bd=2, relief=RIDGE)
+        bg_label.place(x=0, y=160, width=1530, height=710)
 
 
 if __name__ == "__main__":
